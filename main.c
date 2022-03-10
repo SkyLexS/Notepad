a336a5f38c46371b9f379f25105edb30687e1955
SkyLexS <100614316+SkyLexS@users.noreply.github.com>
2022-03-10 02:53:32 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
 {while(1!=0)
 {   printf("[==NOTES==]\n");
     printf("1.Fila noua\n");
-    printf("2.Modificati notita\n");
+    printf("2.Optiuni\n");
     printf("3.Salvati notita\n");
     printf("4.Stergere\n");
 int alegere=0;
@@ -17,23 +17,49 @@ system("cls");
 if(alegere == 1)
     printf("[==FILA NOUA\n==]");
 else if(alegere == 2)
-    {printf("[==MODIFICATI NOTITA==]\n");
-    printf("1.Ionutcuceritorul.txt\n");
-    printf("2.Ip-uri.txt\n");
-    printf("3.Numere de telefon.txt\n");
-    printf("4.Johnutz.txt\n");
-    int alegere_notita=0;
-    while(alegere_notita<1 || alegere_notita>4)
-        {printf("Introdu notita ce doresti sa o modifici:");
-        scanf("%d",&alegere_notita);
-
+    {printf("[==OPTIUNI==]\n");
+    printf("1.Cautare fila\n");
+    printf("2.Modificare fila\n");
+    int alegere_optiune=0;
+    while(alegere_optiune<1 || alegere_optiune>3)
+        {printf("Alege optiunea dorita:");
+        scanf("%d",&alegere_optiune);
         }
+        system("cls");
+        if(alegere_optiune==1)
+            {printf("[==CAUTARE FILA==]\n");
+            char str_nume_fila[50];
+            while(str_nume_fila)
+            {printf("Introdu numele filei pe care doresti sa o cauti:");
+            scanf("%s",&str_nume_fila);
+            }
+            }
+        else if (alegere_optiune==2)
+            {printf("[==MODIFICARE FILA==]\n");
+            printf("1.Ionutcuceritorul.txt\n");
+            printf("2.Ip-uri.txt\n");
+            printf("3.Numere de telefon.txt\n");
+            printf("4.Johnutz.txt\n");
+            int mod_fila=0;
+            while(mod_fila<1 || mod_fila>5)
+            {printf("Introdu numarul notitei pe care doresti sa o modifici:");
+            scanf("%d",&mod_fila);
+            }
+            }
     }
 else if(alegere == 3)
     {printf("[==SALVATI NOTITA DORITA==]\n");}
 else
     {printf("[==STERGERE==]\n");
-    system("pause");
+        printf("1.Ionutcuceritorul.txt\n");
+        printf("2.Ip-uri.txt\n");
+        printf("3.Numere de telefon.txt\n");
+        printf("4.Johnutz.txt\n");
+    int ster_fila=0;
+    while(ster_fila<1 || ster_fila>5)
+    {printf("Introduceti numarul notitei pe care doriti sa o stergeti:");
+    scanf("%d",&ster_fila);
+    }
     }
     system("cls");
 }
